61a031129f0274f4019923343d22737ddeee8dea
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2022-04-23 14:19:21 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,29 +7,7 @@ function banner_resize() {
     });
 }
 
-
-// открытие и закрытие "подробнее"
-// function openCloseMore() {
-//     const openBtns = document.querySelectorAll(".idc-open-btn");
-//     for (let openBtn of openBtns) {
-//         openBtn.addEventListener("click", function (e) {
-//             e.preventDefault();
-//             let id = openBtn.getAttribute("href");
-//             let moreParent = document.querySelector(id);
-//             let more = moreParent.querySelector(".idc-more");
-//             let closeBtns = more.querySelector(".idc-close-btn");
-
-//             if (!more.classList.contains("open")) {
-//                 more.classList.add("open");
-//                 closeBtns.addEventListener("click", function () {
-//                     more.classList.remove("open");
-//                 });
-//             }
-//         });
-//     }
-// }
-
-// открытие и закрытие "списков вакансий"
+// // открытие и закрытие "списков вакансий" и выбор городов
 // function openCloseVacancy() {
 //     const burgerBtns = document.querySelectorAll(".idc-btn-burger");
 //     for (let burgerBtn of burgerBtns) {
@@ -37,51 +15,46 @@ function banner_resize() {
 //             e.preventDefault();
 //             burgerBtn.classList.toggle("open");
 //         });
-
 //     }
+//     window.addEventListener("click", (e) => {
+//         // при клике в любом месте окна браузера
+//         const target = e.target; // находим элемент, на котором был клик
+//         if (
+//             !target.closest(".idc-burger") &&
+//             !target.closest(".idc-btn-burger")
+//         ) {
+//             for (let burgerBtn of burgerBtns) {
+//                 burgerBtn.classList.remove("open");
+//             }
+//         }
+//     });
 // }
+
 // открытие и закрытие "списков вакансий" и выбор городов
-function openCloseVacancy() {
-    const burgerBtns = document.querySelectorAll(".idc-btn-burger");
-    for (let burgerBtn of burgerBtns) {
-        burgerBtn.addEventListener("click", function (e) {
-            e.preventDefault();
+const burgerBtns = document.querySelectorAll(".idc-btn-burger");
+for (let burgerBtn of burgerBtns) {
+    burgerBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        const burgerOpen = document.querySelector(".idc-btn-burger.open");
+        if (burgerOpen && e.target.closest(".idc-btn-burger") != burgerOpen) {
+            burgerOpen.classList.remove("open");
             burgerBtn.classList.toggle("open");
-        });
-    }
-    window.addEventListener("click", (e) => {
-        // при клике в любом месте окна браузера
-        const target = e.target; // находим элемент, на котором был клик
-        if (
-            !target.closest(".idc-burger") &&
-            !target.closest(".idc-btn-burger")
-        ) {
-            for (let burgerBtn of burgerBtns) {
-                burgerBtn.classList.remove("open");
-            }
-        }
+        } else burgerBtn.classList.toggle("open");
     });
-
-    const vacancyNames = document.querySelectorAll(
-        ".idc-burger__item .idc-text-6"
-    );
-
-    for (let vacancyName of vacancyNames) {
-        vacancyName.addEventListener("click", function () {
-            let btnName = document.querySelector(".idc-btn-burger.open .idc-btn-text");
-            btnName.textContent = ''
-            btnName.textContent = vacancyName.textContent;
-            console.log(btnName);
-        });
-    }
 }
+window.addEventListener("click", (e) => {
+    // при клике в любом месте окна браузера
+    const target = e.target; // находим элемент, на котором был клик
+    if (!target.closest(".idc-burger") && !target.closest(".idc-btn-burger")) {
+        for (let burgerBtn of burgerBtns) {
+            burgerBtn.classList.remove("open");
+        }
+    }
+});
 
 document.onreadystatechange = function () {
     window.addEventListener("resize", function () {
         banner_resize();
     });
     banner_resize();
-};
-window.onload = function () {
-    openCloseVacancy();
 };
